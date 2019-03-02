2690933754b42a8703ecf9d4beb870647e7cd11a
Nic Fan <nicfan@gmail.com>
2019-03-02 18:58:46 -0500

    Incorporated gh-pages

--- src/components/ProjectDetail/index.jsx
+++ src/components/ProjectDetail/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './index.scss';
 import propTypes from 'prop-types';
-import Fade from 'react-reveal/Fade';
 import Markdown from 'markdown-to-jsx';
 
 class ProjectDetail extends React.Component {
