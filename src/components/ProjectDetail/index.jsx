71440904605660543092f24f01f64f9719bf73f6
Nic Fan <nicfan@gmail.com>
2019-03-02 20:06:42 -0500

    Revert "Incorporated gh-pages"
    This reverts commit 2690933754b42a8703ecf9d4beb870647e7cd11a.

--- src/components/ProjectDetail/index.jsx
+++ src/components/ProjectDetail/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './index.scss';
 import propTypes from 'prop-types';
+import Fade from 'react-reveal/Fade';
 import Markdown from 'markdown-to-jsx';
 
 class ProjectDetail extends React.Component {
